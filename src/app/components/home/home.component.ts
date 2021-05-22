17938a98e4dfd3cbb7c69317697e922e40202f7e
Ovidijus <mykovidijus@gmail.com>
2021-05-22 19:17:39 +0300

    Interceptors/API

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { APIResponse, Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  public sort: string | undefined;
-  constructor() { }
+  public sort!: string;
+  public games: Array<Game> | undefined;
+  constructor(
+    private httpSevice: HttpService,
+    private activatedRoute: ActivatedRoute) { }
+
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params: Params) => {
+      if(params['games-search']){
+        this.searchGames('metacrit', params['game-search']);
+      } else {
+        this.searchGames('metacrit');
+      }
+    });
+  }
+
+  searchGames(sort: string, search?: string): void {
+    this.httpSevice
+    .getGameList(sort, search).subscribe((gameList: APIResponse<Game>) =>{
+      this.games = gameList.results;
+      console.log(gameList);
+    });
   }
 
 }
